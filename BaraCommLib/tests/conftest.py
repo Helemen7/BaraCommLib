87b4c4d7175b7d122e2ec917006007791bfd4e6f
Matteo Giongo <helemen7.matteo@gmail.com>
2026-05-10 16:13:36 +0200

    Revert "docs(new): Added docs for new features"
    This reverts commit b3c118599dc647ce29f26c9ab73ff8dc06b63e29.

--- BaraCommLib/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os, sys
-# Ensure the package source directory is on PYTHONPATH.
-import os
-base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','src'))
-sys.path.insert(0, base_dir)
